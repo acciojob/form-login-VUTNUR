18138ba1bbee5ca57562457b26ca145933ee8e9a
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-24 09:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
     //Write your code here
   let fname=document.getElementsByTagName("input")[0].value;
 	let lname=document.getElementsByTagName("input")[1].value;
-	alert("first name:"+fname+" and "+"last name:"+lname)
+	alert(fname+lname)
 }
